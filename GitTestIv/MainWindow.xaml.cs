49354a19de7e05c3609d6111d46561a81e0d2d37
idellobel <ivan.dellobel@gmail.com>
2017-09-19 14:30:04 +0200

    bericht fr

--- GitTestIv/MainWindow.xaml.cs
+++ GitTestIv/MainWindow.xaml.cs
@@ -29,6 +29,7 @@ namespace GitTestIv
         {
             lbBericht.Items.Add("Welkom");
             lbBericht.Items.Add("Welcome");
+            lbBericht.Items.Add("Bienvenue");
         }
     }
 }
